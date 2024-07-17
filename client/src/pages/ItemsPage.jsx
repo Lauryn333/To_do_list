1e57b9cf726544f2adacd37e03e34d8132050c65
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-07-17 10:04:42 +0200

    Affichage des éléments de la liste --> pb pas en fonction de la list_id

--- client/src/pages/ItemsPage.jsx
+++ client/src/pages/ItemsPage.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "../styles/items_pages.css";
+
 function ItemsPage() {
-  return <p>test</p>;
+  const items = useLoaderData();
+  const { id } = useParams();
+  const [prep, setPrep] = useState("");
+
+  useEffect(() => {
+    const itemsData = items.filter((item) => item.id === parseInt(id, 10));
+    if (itemsData) {
+      setPrep(itemsData);
+    }
+  }, [id, items]);
+
+  return <p>{prep[0].todo}</p>;
 }
 
 export default ItemsPage;
