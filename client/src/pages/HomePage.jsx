f149eff87303f0fff9ebde499290486e597df988
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-10-10 19:53:35 +0200

    Style mobile page home

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "react-router-dom";
 import logoDailyPlanner from "../assets/images/logoDailyPlanner.png";
+import gitHub from "../assets/images/github.png";
 import "../styles/home_pages.css";
 
 function HomePage() {
@@ -9,7 +10,11 @@ function HomePage() {
   return (
     <section className="home-page">
       <header className="header-hp">
-        <img src={logoDailyPlanner} alt="Logo Daily Planner" />
+        <img
+          className="img-logo"
+          src={logoDailyPlanner}
+          alt="Logo Daily Planner"
+        />
         <h1>Daily Planner</h1>
       </header>
       <section className="lists">
@@ -21,6 +26,7 @@ function HomePage() {
           </Link>
         ))}
       </section>
+      <img className="logo-gitHub" src={gitHub} alt="Logo GitHub" />
     </section>
   );
 }
